d9e7376e3c944f6a8ee8c836b6953f1fd32b8d3b
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-29 18:15:38 +0300

    Update info-contacto.js

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -5,20 +5,20 @@ var handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => '')
 let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografia = await conn.fetchStatus('967776643812' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('967776643812' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('5214434703586'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
+[devnum, `${await conn.getName('967776643812'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://whatsapp.com/channel/0029Vag5yfZJf05mDF9kcU2w`, biobot]
 ], m)
   
 }
-handler.help = ['owner', 'contacto', 'creador', 'contactos']
+handler.help = ['المطور', 'contacto', 'creador', 'contactos']
 handler.tags = ['info']
-handler.command = /^(owner|contacto|creador|contactos)/i
+handler.command = /^(المطور|contacto|creador|contactos)/i
 
 handler.register = true
 
