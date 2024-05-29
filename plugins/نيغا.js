5558d2364c203cfd825838a2a08fc47f38329744
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-30 01:55:37 +0300

    Update نيغا.js

--- plugins/نيغا.js
+++ plugins/نيغا.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'https://telegra.ph/file/caafdb2292db8180b99fe.mp4', 'video.mp4' , message, m);
 };
 
-handler.customPrefix = /^(نيغا|بوت)$/i;
+handler.customPrefix = /^(نيغا|بوت|زنجي|يازنجي)$/i;
 handler.command = new RegExp;
 
 export default handler;
