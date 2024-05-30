94614441615cb849d007dc146eadeed57a01cd13
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-30 15:33:39 +0300

    Update bot-reporte.js

--- plugins/bot-reporte.js
+++ plugins/bot-reporte.js
@@ -1,18 +1,18 @@
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Escriba su reporte*\n\nEjemplo, !${command} el comando !infobot no funciona`, m, fake, )
-if (text.length < 10) return conn.reply(m.chat, `🚩 *Mínimo 10 caracteres para hacer el reporte*`, m, fake, )
-if (text.length > 1000) return conn.reply(m.chat, `🚩 *Máximo 1000 caracteres para hacer el reporte.*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *اكتب تقريرك*\n\nEjemplo, !${command} el comando !infobot no funciona`, m, fake, )
+if (text.length < 10) return conn.reply(m.chat, `🚩 *الحد الأدنى 10 أحرف لإعداد التقرير*`, m, fake, )
+if (text.length > 1000) return conn.reply(m.chat, `🚩 *الحد الأقصى 1000 حرف لعمل التقرير.*`, m, fake, )
 
-let teks = `⚠️ *Reporte* ⚠️\n\n⬡ *Numero*\nWa.me/${m.sender.split`@`[0]}\n\n⬡ *Mensaje*\n${text}`
-conn.reply('5217294888993@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
+let teks = `⚠️ *المطور لوسيفار* ⚠️\n\n⬡ *الرقم*\nWa.me/${m.sender.split`@`[0]}\n\n⬡ *المشكله*\n${text}`
+conn.reply('967776643812@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
 
-conn.reply(m.chat, `🚩 *El reporte se envió a mi creador, tendrá una respuesta más tarde*`, m, fake, )
+conn.reply(m.chat, `🚩 *تم إرسال التقرير إلى منشئ المحتوى الخاص بي, سيكون لها إجابة في وقت لاحق*`, m, fake, )
 
 }
-handler.help = ['reporte', 'request']
+handler.help = ['ابلاغ', 'ابلاغ']
 handler.tags = ['bot']
-handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i
+handler.command = /^(ابلاغ|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i
 
 handler.register = true
 
