0c11f0639b31743e51d4ff8750672f044647eb20
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-03 21:53:39 +0300

    Update ai-hd.js

--- plugins/ai-hd.js
+++ plugins/ai-hd.js
@@ -5,37 +5,37 @@ var handler = async (m, { conn, usedPrefix, command }) => {
 
 conn.hdr = conn.hdr ? conn.hdr : {}
 if (m.sender in conn.hdr)
-return conn.reply(m.chat, `*🏴 Todavía hay una solicitud en proceso, sea paciente*`, m, fake, )
+return conn.reply(m.chat, `*🏴 لا يزال هناك تطبيق في العملية ، كن صبورًا*`, m, fake, )
 
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
 if (!mime)
 
-return conn.reply(m.chat, `*🎌 Etiquete a una foto*`, m, fake, )
+return conn.reply(m.chat, `*🎌 الصورة*`, m, fake, )
 if (!/image\/(jpe?g|png|mp4)/.test(mime))
 
-return conn.reply(m.chat, `*🚩 Mímica ${mime} no soportada*`, m, fake, )
+return conn.reply(m.chat, `*🚩 تشابة ${mime} غير مدعوم*`, m, fake, )
 else conn.hdr[m.sender] = true
 
-conn.reply(m.chat, '⏰ Espere un momento', m, fake, )
+conn.reply(m.chat, '⏰ انتظر لحظه', m, fake, )
 let img = await q.download?.()
 
 let error
 try {
 const This = await processing(img, "enlace")
-conn.sendFile(m.chat, This, '', '🧃 *Toma tu foto*\n\n' + cred.toString('utf-8'), m)
+conn.sendFile(m.chat, This, '', '🧃 *تم تنفيذ طلبك*\n\n' + cred.toString('utf-8'), m)
 } catch (er) {
 error = true
 } finally {
 if (error) {
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
+return conn.reply(m.chat, `*🚩 حدث فشل*`, m, fake, )
 }
 delete conn.hdr[m.sender]
 }}
   
-handler.help = ['hd']
+handler.help = ['جودة']
 handler.tags = ['ai']
-handler.command = /^(hd)$/i
+handler.command = /^(جودة)$/i
 
 handler.register = true
 handler.limit = true
