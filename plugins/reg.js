d1adc98e0746165ea6f89119c94902ec85f24029
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-03 21:37:26 +0300

    Update reg.js

--- plugins/reg.js
+++ plugins/reg.js
@@ -6,15 +6,15 @@ let user = global.db.data.users[m.sender]
 let name2 = conn.getName(m.sender)
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')
-if (user.registered === true) throw `*⚠️ Ya estás registrado*\n\n¿Quiere volver a registrarse?\n\n💬 Use este comando para *eliminar su registro*\n*${usedPrefix}unreg* <Número de serie>`
-if (!Reg.test(text)) throw `*⚠️ Formato incorrecto*\n\n📝 Uso del comamdo: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18`
+if (user.registered === true) throw `*⚠️ انت مسجل مسبقا*\n\n¿هل تريد التسجيل?\n\n💬 استخدم هاذا الامر *.تسجيل+عدنان+العمر*\n*${usedPrefix}unreg* <الرقم السري>`
+if (!Reg.test(text)) throw `*⚠️ تنسيق غير صحيح*\n\n📝 باستخدام الأمر: *${تسجيل + عدنان} الاسم.العمر*\n💡 مثال : *${usedPrefix + command}* ${name2}.19`
 let [_, name, splitter, age] = text.match(Reg)
-if (!name) throw '*📝 El nombre no puede estar vacío*'
-if (!age) throw '*📝 La edad no puede estar vacía*'
-if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*' 
+if (!name) throw '*📝 لا يمكن أن يكون الاسم فارغًا*'
+if (!age) throw '*📝 لا يمكن أن يكون العمر فارغًا*'
+if (name.length >= 30) throw '*⚠️ الاسم طويل جدأ*' 
 age = parseInt(age)
-if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar al bot*'
-if (age < 5) throw '*👀 hay un bebé jsjsj*'
+if (age > 100) throw '*👴🏻 واو الجد يريد أن يلعب دور الروبوت*'
+if (age < 5) throw '*👀 مرحبا بك*'
 user.name = name.trim()
 user.age = age
 user.regTime = + new Date
@@ -22,9 +22,9 @@ user.registered = true
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
 m.react('📩') 
 let regbot = `🗃️ *𝐁𝐎𝐓-𝗟𝐎𝐒𝐄𝐅𝐀𝐑* 🗃️
-🪁 *Nombre:* ${name}
-🎨 *Edad* : ${age} años
-🥏 *Numero de serie*:
+🪁 *الاسم:* ${name}
+🎨 *العمر* : ${age} años
+🥏 *الرقم السري*:
 ${sn}`
 await m.reply(regbot)
 // await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
